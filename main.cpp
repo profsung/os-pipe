9f0d91546c085fbd805ca3eeca6b75dafa04769e
Hong Sung <hs@cs.uco.edu>
2020-09-10 13:50:46 -0500

    update

--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-using namespace std;
-#include <unistd.h>
-#include <string.h>
+#include <unistd.h> // fork pipe
+#include <string.h> // strlen
+#include <stdio.h> // printf
+#include <stdlib.h> // exit
 #define MAX 256
+#define RD_END 0
+#define WR_END 1
 
 int main() {
 	// create a pipe
 	int fd[2];
 	if (pipe(fd) < 0) {
-		cerr << "cannot create a pipe." << endl;
+		printf("cannot create a pipe.\n");
 		exit(1);
 	}
 
 	pid_t pid = fork();
 	if (pid < 0) {
-		cerr << "cannot fork()" << endl;
+		printf("cannot fork()\n");
 		exit(1);
 	} else if (pid == 0) { // child process
-		string data;
+		char data[MAX];
 		while (true) {
-			cout << "Enter a word: ";	
-			getline(cin, data);
-			const char* c_string = data.c_str();
-			write(fd[1], c_string, strlen(c_string) + 1);
+			printf("CHILD: Enter a message: ");
+			fgets(data, MAX, stdin);
+			write(fd[WR_END], data, strlen(data) + 1);
 		}
 		exit(0);
 
 	} else { // parent process
 		char data[MAX];
 		while (true) {
-			read(fd[0], data, sizeof(data));
-			string rd_string(data);
-			cout << "\n\t\t\tMessage received: " << data << endl;
+			read(fd[RD_END], data, sizeof(data));
+			printf("\n\t\t\tPARENT received: %s", data);
 		}
 
 	}
